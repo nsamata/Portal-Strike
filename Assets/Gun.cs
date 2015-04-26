a35cd2991f55fabe92cd7e019547e1ed8709b029
Aashik Ramachandrappa <rmchndr4@illinois.edu>
2015-04-26 14:50:32 -0500

    Map and player created - 1

---
+++ Assets/Gun.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		GameObject player = GameObject.Find ("Player");
+		GameObject guy1 = GameObject.Find ("guy2");
+		float px = player.transform.position.x;
+		float py = player.transform.position.y;
+		float pz = player.transform.position.z;
+		float dir = player.transform.eulerAngles.y;
+		//Vector3 pos = Vector3(px+2, py+1, pz);
+		
+		guy1.transform.position = new Vector3(px, py, pz);
+		guy1.transform.eulerAngles = player.transform.eulerAngles;
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.Find ("Player");
+		GameObject gun = GameObject.Find ("Gun");
+		float px = player.transform.position.x;
+		float py = player.transform.position.y;
+		float pz = player.transform.position.z;
+		float dir = player.transform.eulerAngles.y;
+		//Vector3 pos = Vector3(px+2, py+1, pz);
+		gun.transform.position = new Vector3(px, py, pz);
+		gun.transform.eulerAngles = player.transform.eulerAngles;
+	}
+}
